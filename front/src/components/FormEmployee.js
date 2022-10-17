55d955e437f6bef0af8acc54677780dcfb426a23
Teddys09 <ted3009@hotmail.fr>
2022-10-17 17:14:35 +0200

    Css on modal + create npm package for selectedMenu and install it

--- front/src/components/FormEmployee.js
+++ front/src/components/FormEmployee.js
@@ -3,12 +3,14 @@ import { State } from '../utils/state';
 import { Department } from '../utils/department';
 import { useDispatch, useSelector } from 'react-redux';
 import { addEmployee } from '../utils/store';
-import SelectMenu from './SelectMenu';
+//import SelectMenu from './SelectMenu';
+//import SelectMenu from 'select_menu_teddystrumilo_oc/SelectMenu';
+import SelectMenu from 'select_menu_strumilo_oc/build/index';
 
 const FormEmployee = () => {
   const dispatch = useDispatch();
   const firstName = useSelector((state) => state.employees.employee);
-  console.log(firstName);
+  // console.log(firstName);
   // console.log(firstName);
   function handleSubmit(e) {
     e.preventDefault();
@@ -42,10 +44,16 @@ const FormEmployee = () => {
       dispatch(addEmployee(formValues));
     }
   }
-  const test = {
-    name: 'testMenu',
-    id: 'testMenu',
-    options: ['test1', 'test2', 'test3'],
+
+  const state = {
+    name: 'state',
+    id: 'state',
+    options: State,
+  };
+  const department = {
+    name: 'department',
+    id: 'department',
+    options: Department,
   };
 
   return (
@@ -70,42 +78,30 @@ const FormEmployee = () => {
         <label htmlFor="startDate">Start Date</label>
         <input type="date" name="startDate" id="startDate" />
       </div>
-
-      <div className="address-group">
+      <div className="wrapper-address">
         <p className="address">Address</p>
-        <div className="form-group">
-          <label htmlFor="street">Street</label>
-          <input type="text" name="street" id="street" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="city">City</label>
-          <input type="text" name="city" id="city" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="state">State</label>
-          <select className="form-control" id="state">
-            {State.map((state) => (
-              <option key={state} value={state}>
-                {state}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="zip">Zip Code</label>
-          <input type="text" name="zip" id="zip" />
+        <div className="address-group">
+          <div className="form-group">
+            <label htmlFor="street">Street</label>
+            <input type="text" name="street" id="street" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="city">City</label>
+            <input type="text" name="city" id="city" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="state">State</label>
+            <SelectMenu {...state} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="zip">Zip Code</label>
+            <input type="text" name="zip" id="zip" />
+          </div>
         </div>
       </div>
       <div className="form-group">
         <label htmlFor="department">Department</label>
-        <select className="form-control" id="department">
-          {Department.map((department) => (
-            <option key={department} value={department}>
-              {department}
-            </option>
-          ))}
-        </select>
-        <SelectMenu {...test} />
+        <SelectMenu {...department} />
       </div>
       <input type="submit" className="save-button" value="Save" />
     </form>
